cacb9624769133b0bec85c9143ca32b2a6ee7fb3
onurkaymak <onurkaymak@outlook.com>
2023-09-05 15:56:30 -0700

    Reflect API results to DOM in printResults function.

--- src/js/printResults.js
+++ src/js/printResults.js
@@ -1,3 +1,20 @@
 export const printResults = (data) => {
-    console.log(data);
+    const title = document.getElementById("title");
+    const releasedYear = document.getElementById("released-year");
+    const rate = document.getElementById("rate");
+    const genre = document.getElementById("genre");
+    const writer = document.getElementById("writer");
+    const actors = document.getElementById("actors");
+    const country = document.getElementById("country");
+    const awards = document.getElementById("awards");
+
+    title.innerText = data.Title;
+    releasedYear.innerText = data.Year;
+    rate.innerText = data.Rated;
+    genre.innerText = data.Genre;
+    writer.innerText = data.Writer;
+    actors.innerText = data.Actors;
+    country.innerText = data.Country;
+    awards.innerText = data.Awards;
+
 };
