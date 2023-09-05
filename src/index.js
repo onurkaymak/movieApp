a9751286ee003bba936a47c3056db5a07edea2a8
onurkaymak <onurkaymak@outlook.com>
2023-09-05 14:55:13 -0700

    refactor formSubmitHandler, add form reset feature after every form submission.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import { fetcher } from "./js/business";
 
 const formSubmitHandler = () => {
     event.preventDefault();
-    const input = document.getElementById("input").value;
-    fetcher(input);
+    const input = document.getElementById("input");
+    fetcher(input.value);
+    input.value = "";
 };
 
 window.addEventListener("load", function () {
