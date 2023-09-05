18496038f1583fda3d92147dc021fbeecefafe21
onurkaymak <onurkaymak@outlook.com>
2023-09-05 14:54:30 -0700

    Create fetcher function

--- src/js/business.js
+++ src/js/business.js
@@ -1,3 +1,12 @@
-export const testFunc = () => {
+export const fetcher = (movieName) => {
+    let request = new XMLHttpRequest();
+    const url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${movieName}`;
 
+    request.addEventListener("loadend", function () {
+        const response = JSON.parse(this.responseText);
+        console.log(response);
+    });
+
+    request.open("GET", url, true);
+    request.send();
 };
