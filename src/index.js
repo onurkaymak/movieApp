18c0c664a0d204a5b6431ddc06d7730f7904c462
onurkaymak <onurkaymak@outlook.com>
2023-09-05 14:45:23 -0700

    Create event listener for window and formSubmitHandler function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import { testFunc } from './js/business';
-import './css/styles.css';
+import { fetcher } from "./js/business";
 
+const formSubmitHandler = () => {
+    event.preventDefault();
+    const input = document.getElementById("input").value;
+    fetcher(input);
+};
 
-window.addEventListener('load', testFunc);
+window.addEventListener("load", function () {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", formSubmitHandler);
+});
