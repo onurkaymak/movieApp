b253217142d66b9016503c118cfa939af48f172f
onurkaymak <onurkaymak@outlook.com>
2023-09-07 14:15:26 -0700

    Refactor, create a Movie class with static method for an API call
    Co-authored-by: ian-bravo <ian-bravo@users.noreply.github.com>

---
+++ src/js/movie.js
@@ -0,0 +1,19 @@
+export default class Movie {
+    static findMovieByName(movieName) {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+            const url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${movieName}`;
+            request.addEventListener("loadend", function () {
+                const response = JSON.parse(this.responseText);
+
+                if (this.status === 200) {
+                    resolve(response);
+                } else {
+                    reject(response);
+                }
+            });
+            request.open("GET", url, true);
+            request.send();
+        });
+    }
+}
