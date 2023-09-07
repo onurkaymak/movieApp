b00cf21fc52d37f5e4d15b4248105ad5b143a2b4
onurkaymak <onurkaymak@outlook.com>
2023-09-07 16:13:55 -0700

    Create a movie a class based on the API result and print it, add an event listener on input to clear the result on search
    Co-authored-by: Ian Bravo <bravo.ian@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import Movie from "./js/movie";
 import { printResults } from "./js/printResults";
+import "./css/styles.css";
 
 function printError(error) {
     document.getElementById("error-message").innerText = error[0].Error;
     document.getElementById("error-title").innerText = error[1].status;
+    document.getElementsByClassName("container-error")[0].removeAttribute("class", "hidden");
 }
 
 const formSubmitHandler = (event) => {
     event.preventDefault();
+    const input = document.getElementById("input");
+
+    input.addEventListener("input", function () {
+        document.getElementById("container-result").setAttribute("class", "hidden");
+    });
+
     const movieName = document.getElementById("input").value;
     document.querySelector('#input').value = null;
 
--- src/js/printResults.js
+++ src/js/printResults.js
@@ -1,20 +1,29 @@
+import Movie from "./movie";
+
 export const printResults = (data) => {
-    const title = document.getElementById("title");
-    const releasedYear = document.getElementById("released-year");
-    const rate = document.getElementById("rate");
-    const genre = document.getElementById("genre");
-    const writer = document.getElementById("writer");
-    const actors = document.getElementById("actors");
-    const country = document.getElementById("country");
-    const awards = document.getElementById("awards");
+    let movie = new Movie();
 
-    title.innerText = data.Title;
-    releasedYear.innerText = data.Year;
-    rate.innerText = data.Rated;
-    genre.innerText = data.Genre;
-    writer.innerText = data.Writer;
-    actors.innerText = data.Actors;
-    country.innerText = data.Country;
-    awards.innerText = data.Awards;
+    movie.title = data.Title;
+    movie.releasedYear = data.Released;
+    movie.rate = data.Rated;
+    movie.genre = data.Genre;
+    movie.writer = data.Writer;
+    movie.actors = data.Actors;
+    movie.country = data.Country;
+    movie.awards = data.Awards;
+    movie.poster = data.Poster;
 
+    console.log(movie);
+
+    document.getElementById("title").innerText = movie.title;
+    document.getElementById("released-year").innerText = movie.releasedYear;
+    document.getElementById("rate").innerText = movie.rate;
+    document.getElementById("genre").innerText = movie.genre;
+    document.getElementById("writer").innerText = movie.writer;
+    document.getElementById("actors").innerText = movie.actors;
+    document.getElementById("country").innerText = movie.country;
+    document.getElementById("awards").innerText = movie.awards;
+    document.getElementById("poster").setAttribute("src", movie.poster);
+
+    document.getElementById("container-result").removeAttribute("class", "hidden");
 };
